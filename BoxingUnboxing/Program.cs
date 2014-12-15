96f2fba0760ffe2a56568a3525a58b21ace76ecf
sithi1990 <sithi1990@live.com>
2014-12-15 18:05:42 +0530

    Today Last

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Boxing
+
+            int x = 10;
+            object o = x;
+
+           
+            
+            Console.WriteLine(Object.ReferenceEquals(x,o));
+            Console.WriteLine(o);
+
+            //unboxing
+            int y = (int)o;
+            Console.WriteLine(y);
+
+            Console.ReadLine();
+
+        }
+    }
+}
